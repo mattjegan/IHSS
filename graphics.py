0c08ee19d07dd72d38bc1ea769b802dfd2581699
tolo137 <matthewj.egan@hotmail.com>
2013-09-04 21:24:03 +1000

    Drew grid and tested font

--- graphics.py
+++ graphics.py
@@ -1,3 +1,49 @@
 import pygame
 from pygame.locals import *
 import sys
+
+from constants import *
+
+def draw(screen):
+    drawBG(screen)
+    ##drawHeader(screen)
+    drawGrid(screen)
+    drawStats(screen)
+
+def drawBG(screen):
+    screen.fill(BLUE)
+
+def drawHeader(screen):
+    pass
+
+def drawGrid(screen):
+
+    maxPlayer = 15
+    boxHeight = (SCREEN_HEIGHT-HEAD_PADDING-FOOT_PADDING)/maxPlayer
+
+    for i in xrange(0, maxPlayer):
+        pygame.draw.rect(screen, BLACK, (40, HEAD_PADDING + boxHeight * i, 1200, boxHeight), 5)
+
+def drawStats(screen):
+
+    maxPlayer = 15
+    boxHeight = (SCREEN_HEIGHT-HEAD_PADDING-FOOT_PADDING)/maxPlayer
+    textPosTop = boxHeight/4
+
+    for i in xrange(0, maxPlayer):
+        writeText(screen, WHITE, "Matthew", (50, HEAD_PADDING + textPosTop + boxHeight * i), 45)
+
+## Displays plain text on screen
+## Used for debugging purposes only
+def writeText(screen, color, text, location, size):
+    ## Initialise font engine
+    pygame.font.init()
+
+    ## Define font and size
+    font = pygame.font.Font(None, size)
+
+    ## Create screen object
+    obj = font.render(text, 1, color)
+
+    ## Render text on screen
+    screen.blit(obj, location)
--- main.py
+++ main.py
@@ -2,7 +2,26 @@ import pygame
 from pygame.locals import *
 import sys
 
+from constants import *
+import graphics
+import processing
+
 def main():
-    pass
+    running = True
+
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                running = False
+
+        # Calculate stats
+
+        # Display graphics
+        graphics.draw(screen)
+
+        pygame.display.update()
 
 if __name__ == "__main__": main()
