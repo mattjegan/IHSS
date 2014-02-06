647f995da8f70a93386ab51cdab17e296be475e8
tolo137 <matthewj.egan@hotmail.com>
2014-02-06 13:57:35 +1100

    Modified cgiOutput to display most recent table

--- programs/cgiOutput.py
+++ programs/cgiOutput.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python
 
+gameNumber = open("gameNumber.txt", "rU")
+num = int(gameNumber.readline())
+gameNumber.close()
+
+tableString = "htmlTables/table" + str(num) + ".html"
+tableFile = open(tableString, "rU")
+tableHTML = [line for line in tableFile]
+tableFile.close()
+
 print "Content-type: text/html"
 print
-print "<title>Test CGI</title>"
-print "<p>Hello World!</p>"
+print "<title>X-League Stats</title>"
+for line in tableHTML:
+    print line
